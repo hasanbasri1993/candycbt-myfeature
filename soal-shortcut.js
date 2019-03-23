0d7c02d015fd7c27e476ae8f2dcefe6530a02a7b
Hasan Basri <hasanbasri1493@gmail.com>
2019-03-23 07:46:45 +0700

    soal-shortcut

---
+++ soal-shortcut.js
@@ -0,0 +1,36 @@
+Mousetrap.bind('enter', function () {
+    loadsoal(<?=$id_mapel . "," . $id_siswa . "," . $no_next . ",1"?>);
+});
+
+Mousetrap.bind('right', function () {
+    loadsoal(<?=$id_mapel . "," . $id_siswa . "," . $no_next . ",1"?>);
+});
+
+Mousetrap.bind('left', function () {
+    loadsoal(<?=$id_mapel . "," . $id_siswa . "," . $no_prev . ",1"?>);
+});
+
+Mousetrap.bind('a', function () {
+    $('#A').click()
+});
+
+Mousetrap.bind('b', function () {
+    $('#B').click()
+});
+
+Mousetrap.bind('c', function () {
+    $('#C').click()
+});
+
+Mousetrap.bind('d', function () {
+    $('#D').click()
+});
+
+Mousetrap.bind('e', function () {
+    $('#E').click()
+});
+
+Mousetrap.bind('space', function () {
+    $('input[type=checkbox]').click()
+    radaragu(<?=$id_mapel.",".$id_siswa.",".$soal['id_soal']?>)
+});
